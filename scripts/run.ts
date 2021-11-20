c9fad57356768efd8cdc958f6fc18407649dfb86
Keenan Jaenicke <kjaenick@gmail.com>
2021-11-20 14:56:01 -0500

    adds base smart contract

---
+++ scripts/run.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+
+const main = async () => {
+  const nftContractFactory = await ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+
+  console.log(`EpicNFT contract deployed to: ${nftContract.address}`);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
