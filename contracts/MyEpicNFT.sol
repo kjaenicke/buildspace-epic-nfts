3b7bd70f5df8a9adc2c88a73b606fc661833c02d
Keenan Jaenicke <kjaenick@gmail.com>
2022-01-12 11:26:28 -0500

    random word generation for nft

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -5,21 +5,89 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "hardhat/console.sol";
+
+import { Base64 } from "./libraries/Base64.sol";
 
 contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    string baseSVG = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    enum WordChoice { First, Second, Third }
+
+    string[] firstWords = ["Tasty", "Excited", "Super", "Big", "Enhanced"];
+    string[] secondWords = ["Cheeseburger", "Waffle", "Pickle", "Tomato", "Carrot"];
+    string[] thirdWords = ["Jimmy", "Charlie", "Rusty", "Karen", "Karl"];
+
     constructor() ERC721 ("SquareNFT", "SQUARE") {
         console.log("This is my NFT contraaaaaact!");
     }
 
+    function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+        // I seed the random generator. More on this in the lesson. 
+        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+
+    function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
+
+    function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
+
     function makeAnEpicNFT() public {
         uint256 newItemId = _tokenIds.current();
-        _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/1XA5");
-        console.log("An NFT w/ID %s has been minted to %s", newItemId, msg.sender);
+        string memory first = pickRandomFirstWord(newItemId);
+        string memory second = pickRandomSecondWord(newItemId);
+        string memory third = pickRandomThirdWord(newItemId);
+        string memory combinedWord = string(abi.encodePacked(first, second, third));
+
+        string memory finalSVG = string(abi.encodePacked(baseSVG, first, second, third, "</text></svg>"));
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        // We set the title of our NFT as the generated word.
+                        combinedWord,
+                        '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
+                        // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                        Base64.encode(bytes(finalSVG)),
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+
+        console.log("\n--------------------");
+        console.log(finalSVG);
+        console.log(finalTokenUri);
+        console.log("--------------------\n");
+
+        _safeMint(msg.sender, newItemId);   
+        _setTokenURI(newItemId, finalTokenUri);
         _tokenIds.increment();
+
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
     }
 }
